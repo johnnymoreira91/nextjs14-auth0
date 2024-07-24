44f94709c8a209a076cde960912adc67a7126a6d
Johnny Moreira <jmoreira@engebras.com.br>
2024-07-24 16:43:08 -0300

    first commit

---
+++ src/profile.jsx
@@ -0,0 +1,18 @@
+// import { useUser } from '@auth0/nextjs-auth0/client';
+
+// export const GET = () => {
+
+//   const { user, isLoading, error} = useUser()
+
+//   if (isLoading) return <div>loading...</div>
+//   if (error) return <div>{error.message}</div>
+
+//   if (user) {
+//     return (
+//       <div>
+//         welcame {user.name}! - <a href='/api/auth/logout'>Logout</a>
+//       </div>
+//     )
+//   }
+
+// }
